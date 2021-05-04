7ea84e8c1232cd437e55b9032244ab59b5e13292
Kokouvi Lucien TOHE <kokouvi.tohe@ipnetinstitute.com>
2021-05-04 14:32:45 +0000

    update

--- config/database.php
+++ config/database.php
@@ -99,7 +99,6 @@ return [
     |--------------------------------------------------------------------------
     |
     | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
     | the migrations on disk haven't actually been run in the database.
     |
     */
